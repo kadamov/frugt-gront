c192adb793e3de154d49fec30c40fa0665db10c6
Konstantin <kadamov.work@gmail.com>
2019-09-08 01:54:47 +0300

    fixing nav bar

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
 var tabs = $('.tabs');
-var selector = $('.tabs').find('a').length;
-//var selector = $(".tabs").find(".selector");
+var items = $('.tabs').find('a').length;
+var selector = $(".tabs").find(".selector");
 var activeItem = tabs.find('.active');
 var activeWidth = activeItem.innerWidth();
 $(".selector").css({
-  "left": activeItem.position.left + "px",
+  "left": activeItem.position.left + "px", 
   "width": activeWidth + "px"
 });
 
-$(".tabs").on("click","a",function(e){
-  e.preventDefault();
+$(".tabs").on("click","a",function(){
   $('.tabs a').removeClass("active");
   $(this).addClass('active');
   var activeWidth = $(this).innerWidth();
   var itemPos = $(this).position();
   $(".selector").css({
-    "left":itemPos.left + "px",
+    "left":itemPos.left + "px", 
     "width": activeWidth + "px"
   });
 });
