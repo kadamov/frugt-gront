053411181a2fcf68a65b920a034d193dae2165f3
Konstantin <kadamov.work@gmail.com>
2019-09-09 04:38:28 +0300

    Starting Aabout us page

--- js/main.js
+++ js/main.js
@@ -19,6 +19,21 @@ $(".tabs").on("click","a",function(){
   });
 });
 
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 3000); // Change image every 2 seconds
+}
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
  
