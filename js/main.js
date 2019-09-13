ddc7d323ec62ccb5d484874d2f086f1d969a902a
Konstantin <kadamov.work@gmail.com>
2019-09-13 07:17:31 +0300

    pages code formating

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-//navigation 
+//navigation
 var tabs = $('.tabs');
 var items = $('.tabs').find('a').length;
 var selector = $(".tabs").find(".selector");
 var activeItem = tabs.find('.active');
 var activeWidth = activeItem.innerWidth();
 $(".selector").css({
-  "left": activeItem.position.left + "px", 
-  "width": activeWidth + "px"
+"left": activeItem.position.left + "px",
+"width": activeWidth + "px"
 });
 
 $(".tabs").on("click","a",function(){
-  $('.tabs a').removeClass("active");
-  $(this).addClass('active');
-  var activeWidth = $(this).innerWidth();
-  var itemPos = $(this).position();
-  $(".selector").css({
-    "left":itemPos.left + "px", 
-    "width": activeWidth + "px"
-  });
+$('.tabs a').removeClass("active");
+$(this).addClass('active');
+var activeWidth = $(this).innerWidth();
+var itemPos = $(this).position();
+$(".selector").css({
+"left":itemPos.left + "px",
+"width": activeWidth + "px"
+});
 });
 
 //auto sliding images
@@ -25,25 +25,21 @@ var myIndex = 0;
 carousel();
 
 function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 3000); // Change image every 3 seconds
-}
+var i;
+var x = document.getElementsByClassName("mySlides");
+for (i = 0; i < x.length; i++) { x[i].style.display="none" ; } myIndex++; if (myIndex> x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel, 3000); // Change image every 3 seconds
+    }
 
-//nav mobile opening
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
- 
-  
-}
+    //nav mobile opening
+    function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
 
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
 
-}
+    }
+
+    function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+
+    }
